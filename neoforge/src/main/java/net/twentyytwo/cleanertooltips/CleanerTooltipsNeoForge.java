a15606566a7bde45920d3d8bd111754964349fbc
TwentyyTwo <lennardjbehrens@pm.me>
2026-06-24 06:33:14 +0200

    refactor: replace event with mixin
    - add tooltip icon with mixin instead of event
    
    (cherry picked from commit 50e6e2fa4997ad196171bf0d511de3264abb1382)

--- neoforge/src/main/java/net/twentyytwo/cleanertooltips/CleanerTooltipsNeoForge.java
+++ neoforge/src/main/java/net/twentyytwo/cleanertooltips/CleanerTooltipsNeoForge.java
@@ -2,8 +2,6 @@ package net.twentyytwo.cleanertooltips;
 
 import com.mojang.datafixers.util.Either;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.network.chat.FormattedText;
-import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
@@ -24,10 +22,10 @@ import net.twentyytwo.cleanertooltips.CleanerTooltips.IconAttributeTooltip;
 import net.twentyytwo.cleanertooltips.CleanerTooltips.IconDurabilityComponent;
 import net.twentyytwo.cleanertooltips.CleanerTooltips.IconDurabilityTooltip;
 import net.twentyytwo.cleanertooltips.config.CleanerTooltipsConfig;
+import net.twentyytwo.cleanertooltips.config.CleanerTooltipsConfig.PosValues;
 import net.twentyytwo.cleanertooltips.util.CleanerTooltipsUtil;
 import net.twentyytwo.cleanertooltips.util.AttributeManager;
 
-import java.util.List;
 import java.util.function.Supplier;
 
 import static net.twentyytwo.cleanertooltips.CleanerTooltips.config;
@@ -35,7 +33,6 @@ import static net.twentyytwo.cleanertooltips.CleanerTooltips.config;
 @Mod(value = CleanerTooltips.MOD_ID, dist = Dist.CLIENT)
 @EventBusSubscriber(modid = CleanerTooltips.MOD_ID, value = Dist.CLIENT)
 public class CleanerTooltipsNeoForge {
-    private static ItemStack stackBackup = ItemStack.EMPTY;
 
     public CleanerTooltipsNeoForge(ModContainer container) {
         CleanerTooltips.init();
@@ -67,64 +64,34 @@ public class CleanerTooltipsNeoForge {
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void addTooltip(RenderTooltipEvent.GatherComponents event) {
-        ItemStack stack = event.getItemStack();
-        if (stack.isEmpty()) stack = stackBackup; // Backup if the gui container doesn't provide the stack
+    public static void getMissingComponents(RenderTooltipEvent.GatherComponents event) {
+        PosValues position = config.durability.durabilityPos;
+        if (!config.durability.durabilityEnabled || position == PosValues.INLINE) return;
 
-        if (stack != null && !stack.isEmpty()) {
-            List<Either<FormattedText, TooltipComponent>> tooltipElements = event.getTooltipElements();
-
-            boolean shouldAddAttributes = CleanerTooltipsUtil.canAddAttributeTooltip(stack);
-            boolean shouldAddDurability = CleanerTooltipsUtil.canAddDurabilityTooltip(stack);
-
-            if (!(shouldAddAttributes || shouldAddDurability)) return;
-
-            int insertIndex = getIndex(tooltipElements);
-            if (shouldAddAttributes) {
-                tooltipElements.add(insertIndex, Either.right(new IconAttributeComponent(stack)));
-                insertIndex++;
-            }
-
-            if (shouldAddDurability) {
-                switch (config.durability.durabilityPos) {
-                    case INLINE -> {
-                        if (!shouldAddAttributes) {
-                            tooltipElements.add(insertIndex, Either.right(new IconDurabilityComponent(stack)));
-                        }
-                    }
-                    case BELOW -> tooltipElements.add(insertIndex, Either.right(new IconDurabilityComponent(stack)));
-                    case BOTTOM -> tooltipElements.addLast(Either.right(new IconDurabilityComponent(stack)));
-                }
-            }
-        }
-    }
-
-    private static int getIndex(List<Either<FormattedText, TooltipComponent>> elements) {
-        int index = 1;
+        var elements = event.getTooltipElements();
         for (int i = 0; i < elements.size(); i++) {
-            // Check for the first FormattedText element, and get the index after
-            if (elements.get(i).left().isPresent()) {
-                index = i + 1;
-                break;
+            var op = elements.get(i).right();
+            if (op.isEmpty()) continue;
+            if (op.get() instanceof IconAttributeComponent(ItemStack stack)) {
+                if (stack.isDamageableItem()) {
+                    int index = position == PosValues.BELOW ? i + 1 : elements.size();
+                    elements.add(index, Either.right(new IconDurabilityComponent(stack)));
+                }
+                return;
+            } else if (op.get() instanceof IconDurabilityComponent) {
+                if (i != elements.size() - 1 && position == PosValues.BOTTOM) {
+                    elements.add(elements.remove(i));
+                }
+                return;
             }
         }
-
-        // If that element's a TooltipComponent, increment by one
-        while (index < elements.size() && elements.get(index).right().isPresent()) {
-            index++;
-        }
-
-        return index;
     }
 
     @SubscribeEvent()
     public static void hideDefaultAttributes(GatherSkippedAttributeTooltipsEvent event) {
-        stackBackup = event.getStack();
-        event.setSkipAll(CleanerTooltipsUtil.canAddAttributeTooltip(stackBackup));
+        event.setSkipAll(CleanerTooltipsUtil.canAddAttributeTooltip(event.getStack()));
 
         // Don't display mining efficiency if mining speed is displayed
-        if (config.general.miningSpeed) {
-            event.skipId(CleanerTooltipsUtil.EFFICIENCY);
-        }
+        if (config.general.miningSpeed) event.skipId(CleanerTooltipsUtil.EFFICIENCY);
     }
 }

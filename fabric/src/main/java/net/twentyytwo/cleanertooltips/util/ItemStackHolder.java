7b280cefa8b469b682e276c35b78325a0542432d
TwentyyTwo <lennardjbehrens@pm.me>
2026-06-27 22:01:51 +0200

    refactor: fix possible mixin null exception and reduce complexity
    - make WeaponAttributeTooltipMixin abstract

--- fabric/src/main/java/net/twentyytwo/cleanertooltips/util/ItemStackHolder.java
+++
@@ -1,7 +0,0 @@
-package net.twentyytwo.cleanertooltips.util;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface ItemStackHolder {
-    void cleanerTooltips$setStack(ItemStack stack);
-}
